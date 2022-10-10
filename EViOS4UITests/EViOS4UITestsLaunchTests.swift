1d71079810b9d83e88c8b29b44a5a5e42fdc4e8e
Mavrik <r.mavrik@gmail.com>
2022-10-10 08:56:00 +0200

    init commit

---
+++ EViOS4UITests/EViOS4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EViOS4UITestsLaunchTests.swift
+//  EViOS4UITests
+//
+//  Created by Mavrik on 10/10/2022.
+//
+
+import XCTest
+
+final class EViOS4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
